4a057cd013af7b2e5b338900cdd36248d24c44f8
GurvirDehal <gurvirdehal@gmail.com>
2020-02-15 19:46:24 -0700

    Added description filtering.

--- parser.py
+++ parser.py
@@ -33,13 +33,49 @@ def get_desc(course_link, course_code):
     number = get_number()
     parent = soup.find('a', {'name' : number}).parent
     desc = parent.find('span', {'class' : 'course-desc'}).text
-    print(desc)
+    # print(desc)
     return desc
+
+def string_contains(str1,str2):
+    lst1 = str1.split(' ')
+    lst2 = str2.split(' ')
+
+    if len(lst1) <= len(lst2):
+        return lst1 == lst2[:len(lst1)]
+
+    return False
+
+common_pun = [",", ".", ";", ":"]
+common_words = ["the", "to", "of", "and", " a ", "in", "that", "for", "it", "on", "with", "as", "do", "at", "this", "by", "from", "an", "all", "there", "their", "what"]
+def remove_word(desc, wordIndex):
+    if (wordIndex < 0):
+        return desc
+    if (string_contains(common_words[wordIndex], desc)):
+        return remove_word(desc.replace(common_words[wordIndex],''), wordIndex - 1)
+    else: 
+        return remove_word(desc, wordIndex - 1)
     
+
+def remove_pun(desc, punIndex):
+    if(punIndex < 0):
+        return desc
+    arr = []
+    for d in desc:
+        for lol in d:
+            returnValue = lol.split(common_pun[punIndex])
+            arr.append(returnValue)
+
+    return remove_pun(arr, punIndex - 1)
+
 try:
-    get_desc(get_course_link(name), code)
+    description = get_desc(get_course_link(name), code)
+    # d2 = remove_word(description, len(common_words) - 1) # doesn't work rn
+    d3 = remove_pun([[description]], len(common_pun) - 1)
+    d4 = []
+    for i in d3:
+        for j in i:
+            if j:
+                d4.append(j.strip())
+    print(d4)
 except:
     pass
-
-
-        
