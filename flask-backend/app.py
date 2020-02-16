11ee9f56b79dd1386cc80be97f0c6d15865c9994
justinf34 <justin.flores@ucalgary.ca>
2020-02-16 02:34:58 -0700

    We figured out the querying for firebase.

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -5,11 +5,18 @@ from parser import get_key_words
 from youtubeapi import youtube_searchURL
 import json
 
+from firebase_admin import credentials, firestore, initialize_app
+
+from google.cloud import storage, exceptions
+
+
 app = Flask(__name__)
 
-# Connecting to the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://josh_prowse:Prowse123@hackathon-2020.cfwfxw7xy4fu.us-east-2.rds.amazonaws.com/yt_vid_course_compiler'
-db = SQLAlchemy(app)
+# Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+course_ref = db.collection('Courses')
 
 
 @app.route('/')
@@ -19,19 +26,62 @@ def index():
 
 @app.route('/playlist.html')
 def temp():
-    query = request.args.get('search').split(' ')
-    words = get_key_words(query[0], query[1])
-    returnJSON = []
-    for word in words:
-        video_ids = youtube_searchURL(word)
-        y = json.dumps({
-            'keyword': word,
-            'videos': video_ids
-        })
-        returnJSON.append(y)
-        print(returnJSON[0])
-    return render_template("playlist.html", result=returnJSON)
+
+    course_name = request.args.get('search')
+    query = course_name.split(' ')
+
+    has_course = course_ref.document(course_name).get()
+    has_course = has_course.to_dict()
+
+    if has_course is None:
+        words = get_key_words(query[0], query[1])
+        returnJSON = []
+        for word in words:
+            video_ids = youtube_searchURL(word)
+            y = json.dumps({
+                'keyword': word,
+                'videos': video_ids
+            })
+            returnJSON.append(y)
+            print(returnJSON[0])
+    else:
+        return render_template("playlist.html", result=returnJSON)
+
+
+@app.route('/insert')
+def insert():
+    temp = 'STAT 217'
+    # course = course_ref.document(temp).collection(u'Keywords').document(
+    #     u'Estimation of population parameters').collection(u'Videos').document(u'FX51BBGoE68').get()
+    data = {
+        'rating': 0
+    }
+
+    vid_id = 'rIud0ekk-sM'
+
+    keyword = 'confidence intervals for means'
+    course = course_ref.document(temp)
+    course.collection(u'Keywords').document(
+        keyword).collection(u'Videos').document(vid_id).set(data)
+    return "I added :^)"
+    # course_again = course.to_dict()
+    # if course_again is None:
+    #     # course = course_ref.document(temp).collection(u'Keywords').document(
+    #     #     u'Estimation of population parameters').collection(u'Videos').document(u'FX51BBGoE68').get()
+    #     print("I FAILED")
+    #     return "Failed"
+
+    # else:
+    #     print(course_again)
+    #     return "Hello"
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# museums = db.collection_group(u'landmarks')\
+#     .where(u'type', u'==', u'museum')
+# docs = museums.stream()
+# for doc in docs:
+#     print(u'{} => {}'.format(doc.id, doc.to_dict()))
